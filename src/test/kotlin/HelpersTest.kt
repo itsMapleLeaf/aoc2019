54781742c6f4d3be90e5200c5bcd0602d6832cde
kingdaro <kingdaro@gmail.com>
2019-12-05 16:58:13 -0600

    fix test name + inline variable

--- src/test/kotlin/HelpersTest.kt
+++ src/test/kotlin/HelpersTest.kt
@@ -2,8 +2,7 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class HelpersTest : StringSpec({
-    "withValueAtIndex should replace a value at an index" {
-        val result = listOf(1, 2, 3).replace(4, 0)
-        result shouldBe listOf(4, 2, 3)
+    "replace should replace a value at an index" {
+        listOf(1, 2, 3).replace(4, 0) shouldBe listOf(4, 2, 3)
     }
 })
