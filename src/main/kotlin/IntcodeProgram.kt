66b393d9a2d14c1efe1bf770bb08d61ad5a5e838
kingdaro <kingdaro@gmail.com>
2019-12-05 18:54:42 -0600

    process input/output

--- src/main/kotlin/IntcodeProgram.kt
+++ src/main/kotlin/IntcodeProgram.kt
@@ -1,13 +1,31 @@
-internal data class IntcodeProgram(private val values: List<Int>, private val position: Int) {
+internal data class IntcodeProgram(
+    private val values: List<Int>,
+    private val position: Int,
+    private val input: Iterator<Int>,
+    internal val output: List<Int>
+) {
     companion object {
         fun fromString(programString: String): IntcodeProgram {
-            val values = programString.split(',').map { it.toInt() }
-            return IntcodeProgram(values, 0)
+            return IntcodeProgram(
+                values = programString.split(',').map { it.toInt() },
+                position = 0,
+                input = IntegerInputIterator(),
+                output = listOf()
+            )
         }
     }
 
     fun withValue(value: Int, index: Int) =
-        IntcodeProgram(values.replace(value, index), position)
+        IntcodeProgram(values.replace(value, index), position, input, output)
+
+    fun withInput(newInput: Iterator<Int>) =
+        IntcodeProgram(values, position, newInput, output)
+
+    private fun move(steps: Int) =
+        IntcodeProgram(values, position + steps, input, output)
+
+    private fun withOutput(newOutput: Int) =
+        IntcodeProgram(values, position, input, output + newOutput)
 
     private fun getNextProgram(): IntcodeProgram? {
         return when (val currentValue = values[position]) {
@@ -25,9 +43,18 @@ internal data class IntcodeProgram(private val values: List<Int>, private val po
                     else -> throw Error("we broke the laws of physics somehow")
                 }
 
-                val nextValues = values.replace(result, destinationIndex)
+                withValue(result, destinationIndex).move(4)
+            }
 
-                IntcodeProgram(nextValues, position + 4)
+            3 -> {
+                val newValue = input.next()
+                val destinationIndex = values[position + 1]
+                withValue(newValue, destinationIndex).move(2)
+            }
+
+            4 -> {
+                val outputIndex = values[position + 1]
+                withOutput(values[outputIndex]).move(2)
             }
 
             99 ->
@@ -44,4 +71,24 @@ internal data class IntcodeProgram(private val values: List<Int>, private val po
     }
 
     val firstValue get() = values.first()
+}
+
+private class IntegerInputIterator : Iterator<Int> {
+    override fun hasNext() = true
+    override fun next() = getIntegerInput()
+}
+
+private tailrec fun getIntegerInput(): Int {
+    print("enter an integer: ")
+
+    try {
+        val input = readLine() ?: throw Error("no input found?")
+        return input.toInt()
+    } catch (error: NumberFormatException) {
+        println("not a valid integer >:(")
+    } catch (error: Error) {
+        println("an error occurred: ${error.message}")
+    }
+
+    return getIntegerInput()
 }
