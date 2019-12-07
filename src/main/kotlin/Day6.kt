cbc007f6ed110192093eac9bdffa6d22356302a7
kingdaro <kingdaro@gmail.com>
2019-12-07 13:43:14 -0600

    day 6 part 2

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,25 +1,33 @@
 import java.io.File
 
-private class Planet {
+private class Planet(private val name: String) {
     internal var parent: Planet? = null
+
+    override fun toString() = name
 
     internal fun getOrbitCount(): Int =
         if (parent == null) 0 else 1 + parent!!.getOrbitCount()
+
+    internal fun getPathToCenter(): List<Planet> =
+        listOf(this) + if (parent == null)
+            listOf()
+        else
+            parent!!.getPathToCenter()
 }
 
 private fun getInput() =
     File("PlanetChart.txt").readLines()
 
 private fun getTestInput() =
-    "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L".split(" ")
+    "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L K)YOU I)SAN".split(" ")
 
 private fun constructPlanetMap(): Map<String, Planet> {
     val namePairs = getInput().map { it.split(")").toPair() }
     val planetMap = mutableMapOf<String, Planet>()
 
     for ((parentName, childName) in namePairs) {
-        val parent = planetMap[parentName] ?: Planet()
-        val child = planetMap[childName] ?: Planet()
+        val parent = planetMap[parentName] ?: Planet(parentName)
+        val child = planetMap[childName] ?: Planet(childName)
         child.parent = parent
         planetMap[parentName] = parent
         planetMap[childName] = child
@@ -32,15 +40,26 @@ private fun getTotalOrbits() {
     val planetMap = constructPlanetMap()
     val totalOrbits = planetMap.values.map { it.getOrbitCount() }.sum()
 
-    println(totalOrbits)
+    println("total orbits: $totalOrbits")
 }
 
-//private fun getTotalOrbitalTransfersToSanta() {
-//    val planetMap = constructPlanetMap()
-//    println(planetMap["COM"]?.stepsToInChildren(planetMap["D"]!!))
-//}
+private fun getTotalOrbitalTransfersToSanta() {
+    val planetMap = constructPlanetMap()
+
+    val youToCenter = planetMap.getValue("YOU").getPathToCenter().reversed()
+    val sanToCenter = planetMap.getValue("SAN").getPathToCenter().reversed()
+
+    val sharedCount = youToCenter.indices
+        .takeWhile { index -> youToCenter[index] == sanToCenter[index] }
+        .size
+
+    val uniquePaths = youToCenter.drop(sharedCount) + sanToCenter.drop(sharedCount)
+    val totalDistance = uniquePaths.size - 2
+
+    println("minimum orbital transfers to santa: $totalDistance")
+}
 
 fun main() {
     getTotalOrbits()
-//    getTotalOrbitalTransfersToSanta()
+    getTotalOrbitalTransfersToSanta()
 }
