79c5c33ee1ef4ce92be764ba10785cb4cf5b7e3b
kingdaro <kingdaro@gmail.com>
2019-12-07 21:03:47 -0600

    combine helper tests (oops)

--- src/test/kotlin/HelpersTest.kt
+++ src/test/kotlin/HelpersTest.kt
@@ -1,8 +1,16 @@
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
 
 class HelpersTest : StringSpec({
     "replace should replace a value at an index" {
         listOf(1, 2, 3).replace(4, 0) shouldBe listOf(4, 2, 3)
+    }
+
+    "plusAt" {
+        listOf(1, 2, 4, 5).plusAt(2, 3) shouldBe listOf(1, 2, 3, 4, 5)
+        listOf(2, 3).plusAt(0, 1) shouldBe listOf(1, 2, 3)
+        listOf(1, 2).plusAt(2, 3) shouldBe listOf(1, 2, 3)
+        shouldThrow<Error> { listOf(1, 2, 3).plusAt(10, 69) }
     }
 })
