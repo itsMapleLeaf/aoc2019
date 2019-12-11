701b2e8bec9424a29c435c8365e8afd1eaf804b5
kingdaro <kingdaro@gmail.com>
2019-12-10 19:42:39 -0600

    better name?

--- src/main/kotlin/math/Util.kt
+++ src/main/kotlin/math/Util.kt
@@ -3,5 +3,5 @@ package math
 fun delta(value: Number, a: Number, b: Number): Double =
     value.toDouble() / (a.toDouble() + (b.toDouble() - a.toDouble()))
 
-fun Number.isBetween(start: Number, end: Number) =
+fun Number.isInRange(start: Number, end: Number) =
     start.toDouble() <= this.toDouble() && this.toDouble() <= end.toDouble()
